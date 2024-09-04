e7e7bed54f1783fe1cd2e63564f63b2e8df9de93
weedavedev <admin@wallacedevelopment.co.uk>
2024-09-04 18:09:52 +0200

    basic svelte static page structure implemented. Next routing pages and a nav bar.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,svelte,js}'],
+    theme: {
+        extend: {
+            colors: {
+                space: '#48b93f',
+            }
+        },
+    },
+    plugins: [],
+};
